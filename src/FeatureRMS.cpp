cdf99143d3ce9ba877e4f5dd6c885271d27c79ce
jaeseong you <jaesoengyou@gmail.com>
2014-12-09 11:00:50 -0500

    FeatureRMS memory allocation changed

--- src/FeatureRMS.cpp
+++ src/FeatureRMS.cpp
@@ -1,27 +1,35 @@
 FeatureRMS::calculate_feature(){
-			nCols = ceil(bufSize/hopSize);
-			nRows = 1;
-			feautre = calloc(nCol*nRow*sizeof(SAMPLE));
-		
+			
+			// set nCols and nRows
+			nCols = 1;
+			nRows = ceil(bufSize/hopSize);
+			
+			// allocate memory for feature
+			feautre = (SAMPLE**)malloc(nCols*sizeof(SAMPLE*));
+			for (int i = 0; i < nCols; i++){
+				feature[i] = (SAMPLE*)calloc(nRows*sizeof(SAMPLE*));
+			}
+			
+			// RMS calculation
 			if (winSize == 0){ 
 				return 0.0;
 			}
 			else if (winSize > bufSize){
-				printf("Your winSize exceeds bufSize\n");
+				printf("winSize exceeds bufSize\n");
 				return 1.0;
 			}
 			else{
 				SAMPLE squareSum = 0.0
-				for (int i = 0; i < nCol; i += hopSize){
+				for (i = 0; i < nRows; i += hopSize){ // a hop
 					SAMPLE squareSum = 0.0;
 			
-					for (int j = 0; j < winSize; j++){
+					for (int j = 0; j < winSize; j++){ // sum values within window
 						if (i*winSize+j >= bufSize){ // conceptual zero-padding
 							break;
 						}
 						squareSum += signal[i*winSize+j] * signal[i*winSize+j];
 					}
-					feature[i] = sqrt(squareSum / (SAMPLE)winSize);
+					feature[i][nCols] = sqrt(squareSum / (SAMPLE)winSize);
 				}
 			}
 			return feature; 
