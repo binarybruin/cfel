f41f75aa1af55d9a0205204d19bc0b4df947ad64
Jordan Juras <jdj309@nyu.edu>
2014-12-15 16:09:21 -0500

    Modified AudioAnalyzer.cpp,.h and working on MainComponent.cpp bufer flags.

--- src/JUCESource/AudioAnalyzer.cpp
+++ src/JUCESource/AudioAnalyzer.cpp
@@ -7,6 +7,7 @@
 //
 
 #include "AudioAnalyzer.h"
+//#include "FeatureController.h"
 
 
 AudioAnalyzer::AudioAnalyzer()
@@ -44,7 +45,7 @@ void AudioAnalyzer::paint (Graphics& g)
     g.fillAll (Colours::black);
     
     textBox.moveCaretToEnd();
-    textBox.insertTextAtCaret ("RMS: " + str + newLine);
+    textBox.insertTextAtCaret ("RMS:EDIT: " + str + newLine);
 }
 
 void AudioAnalyzer::resized()
@@ -54,12 +55,23 @@ void AudioAnalyzer::resized()
     textBox.setBounds (r);
 }
 
+bool AudioAnalyzer::getflag()
+{
+    return flag;
+}
+
 void AudioAnalyzer::clear()
 {
     zeromem (samples, sizeof (samples));
+    zeromem (saveBuf1, sizeof (saveBuf1)); // ensure the saveBuf is not full of junk, on the first iteration of
+                                            //pushing the buf to featureController
+    zeromem (saveBuf2, sizeof (saveBuf2));
     accumulator = 0;
     subSample = 0;
     iteration = 0;
+    counterBuf = 0;
+    flag = false;
+    //SAMPLE** featVal;
 }
 
 void AudioAnalyzer::audioDeviceIOCallback (const float** inputChannelData, int numInputChannels,
@@ -74,6 +86,8 @@ void AudioAnalyzer::audioDeviceIOCallback (const float** inputChannelData, int n
             if (inputChannelData[chan] != nullptr){
                 
                 inputSave[chan] = inputChannelData[chan][i];
+                                
+                //for RMS calc only (ELSKA)
                 inputSample += std::abs (inputChannelData[chan][i]);  // find the sum of all the channels
         
                 pushSample (10.0f * inputSample, inputSave, numInputChannels); // boost the level to make it more easily visible.
@@ -89,18 +103,73 @@ void AudioAnalyzer::audioDeviceIOCallback (const float** inputChannelData, int n
             inputSave[j] = 0;
         }
     }
-    
 }
 
 
 
 void AudioAnalyzer::pushSample (const float newSample, const float (&inputSave)[2], int numChannels)
 {
-    accumulator += newSample;
+    
+    counterSave = iteration % (numChannels*100);
+    if(counterSave == 0 && iteration > 0){ //iteration > 0, to ensure that that we start saving into saveBuf1
+        counterBuf++;
+    }
+
+    bufSelect = counterBuf % 2;
+    
+    //save to the buffers here
+    if(bufSelect == 0){
+    for (int chan = 0; chan < numChannels; ++chan){
+        saveBuf1[counterSave + chan] = inputSave[chan];
+        }
+    }
+    else{
+        for (int chan = 0; chan < numChannels; ++chan){
+            saveBuf2[counterSave + chan] = inputSave[chan];
+        }
+    }
+    
+    if(counterSave == 0 && bufSelect == 0){
+        flag = true;
+        std::cout << "flag1!" << flag;
+        
+        //TO DO: pass the save buff to feature extraction. it will be of size saveBuf[numChannels * 100 * 1024] (i think ?)//
+        //but for now
+        //std::cout << saveBuf1 << saveBuf1[2] << std::endl << iteration << std::endl;
+        //AudioAnalyzer::pushSample is a void return function because all the FeatureFunction calls are pushed to animation within it, but
+        // RMS = featureControler::CalcRMS(saveBuf); RMS will be the value passed to the animation, but the buffer in the    function will save exterior to this function::pushSample
+        //FFT = featureControler::CalcFFT(saveBuf);
+        //NOTE: as stated above OUR FEATURE:Extraction RETURNS MUST SAVE TO A GLOBAL VARIABLE THAT WILL BE ACCESSED BY THE GRAPHICS RENDERING FUNCTION
+        
+        // featureControllerObject = new FeatureController( INFO );
+        // featVal = featureControlObject.getFeature(RMS);
+     
+        //set saveBuf to zero
+        for(int i = 0; i < (numChannels*100*1024); ++i){
+            saveBuf1[i] = 0;
+        }
+    }
+    else if(counterSave == 0 && bufSelect == 1){
+        flag = false;
+        std::cout << "flag2!" << flag;
+        
+        for(int i = 0; i < (numChannels*100*1024); ++i){
+            saveBuf2[i] = 0;
+        }
+    }        // end of save to buffer
+        
+    for(int i = 0; i < numChannels; i++) {
+        iteration++; //ensures proper indexing of saveBuf, necessitates modulo numChannels*100 because it is counting (numChannels times) as fast.
+    }
+    
+    
+    //push to animation _ _ _ _ _ _ _ _ _ _ need to add different variables for different animation executions
+    
+    accumulator += newSample; //
     
     if (subSample == 0)
     {
-        const int inputSamplesPerPixel = 200;
+        const int inputSamplesPerPixel = numChannels*100;
         
         samples[nextSample] = accumulator / inputSamplesPerPixel;
         nextSample = (nextSample + 1) % numElementsInArray (samples);
@@ -112,29 +181,4 @@ void AudioAnalyzer::pushSample (const float newSample, const float (&inputSave)[
         --subSample;
         
     }
-    
-    itermod = iteration % (numChannels*100);
-    //save to the buffer here
-    for (int chan = 0; chan < numChannels; ++chan){
-        saveBuf[itermod + chan] = inputSave[chan];
-    }
-    
-    
-    if(itermod == 0){//numChannels*100 - 1){
-        std::cout << "flag!";
-        
-        //TO DO: pass the save buff to feature extraction. it will be of size saveBuf[numChannels * 100 * 1024] (i think ?)//
-        //but for now
-        std::cout << saveBuf << iteration << std::endl;
-        
-        //set saveBuf to zero
-        for(int i = 0; i < (numChannels*100*1024); ++i){
-            saveBuf[i] = 0;
-        }
-    } // end of save to buffer
-    
-    for(int i = 0; i < numChannels; i++) {
-        iteration++; //ensures proper indexing of saveBuf, but necessitates modulo 200 because it is counting twice as fast.
-    }
-    
 }
