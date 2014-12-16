4c750146783a2673eabb550808a49d347ec5b63d
Jordan Juras <jdj309@nyu.edu>
2014-12-15 23:34:05 -0500

    zcr added

--- src/featureTest/featureTest.c
+++ src/featureTest/featureTest.c
@@ -15,8 +15,10 @@
     float RMSBuf[BUFFER_SIZE*100]; // is this the correct size ?
     char sineFile[] = "featureTest.txt";
     char RMSFile[] = "RMSTest.txt";
+    char ZCRFile[] = "ZCRTest.txt";
 
-    void featureTest(float buffer[BUFFER_SIZE*100], char fTestfilename[], FILE* fileptr);
+    void featureRMSTest(float buffer[BUFFER_SIZE*100], char fTestfilename[], FILE* fileptr);
+    void featureZCRTest(float buffer[BUFFER_SIZE*100], char fTestfilename[], FILE* fileptr);
 
 int main(){
 
@@ -35,6 +37,14 @@ int main(){
     return EXIT_FAILURE;
   }
 
+   // try to open ZCR output file
+  FILE * zcrptr = fopen(ZCRFile, "w");
+  if (zcrptr == NULL) {
+    printf("Could not open %s for writing.\n", ZCRFile);
+    return EXIT_FAILURE;
+  }
+
+
     // sine wave parameters:
     float sampleSine, phase;
     static float prev_phase = 0;
@@ -51,21 +61,29 @@ int main(){
 
 
     //test features
-    featureTest(sineBuf, RMSFile, rmsptr);
+    featureRMSTest(sineBuf, RMSFile, rmsptr);
+    featureZCRTest(sineBuf, ZCRFile, zcrptr);
 
     fclose(sineptr);
     fclose(rmsptr);
+    fclose(zcrptr);
 
     return EXIT_SUCCESS;
 }
 
 
 
-void featureTest(float buffer[BUFFER_SIZE*100], char fTestfilename[], FILE* fileptr){
+/*
+ * feature function tests  
+ */
+
+
+/* ROOT MEAN SQUARE */
+void featureRMSTest(float buffer[BUFFER_SIZE*100], char fTestfilename[], FILE* fileptr){
 
 /* Parameter initialization */
 int m_bufSize = BUFFER_SIZE*100;
-int m_winSize = m_bufSize/2;
+int m_winSize = m_bufSize/6;
 int m_hopSize = m_winSize/2;
 int m_nRows;
 float **m_feature;
@@ -96,7 +114,7 @@ float *m_signal = buffer;
 				SAMPLE sqrSum = 0.0; // CORRECTION: missing ';' at end of line
 				SAMPLE val;
 				for (i = 0; i < m_nCols; ++i){			
-					for (int j = 0; j < m_nRows; ++j){
+					for (int j = 0; j < m_nRows; ++j){ 
 						for (int k = 0; k < m_winSize; ++k){
 							if (j*m_hopSize+k >= m_nCols){
 								val = 0;
@@ -104,11 +122,70 @@ float *m_signal = buffer;
 							else{
 								val = m_signal[j*m_hopSize+k];
 							}
-							sqrSum += val*val;
+							sqrSum += val*val; 
+							
 						}
-                    }
+					} 
 					m_feature[j][i] = sqrt(sqrSum/(SAMPLE)m_winSize);
 					fprintf(fileptr, "m_feature[%d][%d] = %f\n", j, i, m_feature[j][i]); //ADDED FOR PRINTOUT of m_feature
 				}
+			}
+}
+
+
+
+
+
+/* ZERO CROSSING RATE */
+void featureZCRTest(float buffer[BUFFER_SIZE*100], char fTestfilename[], FILE* fileptr){
+
+/* Parameter initialization */
+int m_bufSize = BUFFER_SIZE*100;
+int m_winSize = m_bufSize/6;
+int m_hopSize = m_winSize/2;
+int m_nRows;
+float **m_feature;
+float *m_signal = buffer;
+
+/* copy and pasted from FeatureRMS.cpp */
+
+            int m_nCols = 1;
+            m_nRows = ceil(m_bufSize/m_hopSize);
+
+            // allocate memory for feature
+			m_feature = (SAMPLE**)malloc(m_nCols*sizeof(SAMPLE*)); // CORRECTION: m_feautre -> m_feature
+			for (int i = 0; i < m_nCols; ++i){
+				m_feature[i] = (SAMPLE*)malloc(m_nRows*sizeof(SAMPLE*));
+			}
+			
+			// ZCR calculation
+			if (m_winSize <= 0){ 
+				printf("Check your winSize.\n");
+				exit(1);
+			}
+			else if (m_winSize > m_bufSize){
+				printf("winSize exceeds bufSize.\n");
+				exit(1);
+			}
+			else{
+			int zcc; // zero crossing count
+				for (i = 0; i < m_nCols; ++i){			
+					for (int j = 0; j < m_nRows; ++j){
+						zcc = 0;
+						for (int k = 0; k < m_winSize; ++k){
+							if (j*m_hopSize+k <= m_nCols){
+								// check the sign of two consecutive samples
+								if (m_signal[j*m_hopSize+k] < 0 && m_signal[j*m_hopSize+k+1] > 0){
+									++zcc;
+								}
+								if (m_signal[j*m_hopSize+k] > 0 && m_signal[j*m_hopSize+k+1] < 0){
+									++zcc;
+								}
+							}
+						}
+						m_feature[j][i] = (SAMPLE)zcc/m_winSize;
+						fprintf(fileptr, "m_feature[%d][%d] = %f\n", j, i, m_feature[j][i]); //ADDED FOR PRINTOUT of m_feature
+					}
+				}	 
 			}
 }
